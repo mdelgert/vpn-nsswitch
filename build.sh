4e724b77f09e39e184213edcc7fd2e44558c5798
Matthew Elgert <mdelgert@yahoo.com>
2025-05-24 23:30:51 -0400

    Refactor build and installation scripts for nsswitch-vpn package; streamline build process and update control files

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-rm nsswitch-vpn_1.0.0.deb
-sudo apt remove nsswitch-vpn -y
-dpkg-deb --build --root-owner-group . nsswitch-vpn_1.0.0.deb
-chmod 644 nsswitch-vpn_1.0.0.deb
-sudo apt install ./nsswitch-vpn_1.0.0.deb
-# nmcli connection up WireGuard
-# nmcli connection down WireGuard
+# sudo apt update
+# sudo apt install devscripts build-essential fakeroot
+
+set -e
+
+# Clean previous build artifacts
+rm -f ../nsswitch-vpn_1.0.0.deb ../nsswitch-vpn_1.0.0_*.build ../nsswitch-vpn_1.0.0_*.changes ../nsswitch-vpn_1.0.0_*.tar.* ../nsswitch-vpn_1.0.0_*.dsc
+
+# Remove installed package if present
+#csudo apt remove nsswitch-vpn -y || true
+
+# Build the Debian package using standard tools
+DEB_BUILD_OPTIONS=noautodbgsym dpkg-buildpackage -us -uc
+
+# Install the newly built package
+sudo apt install ../nsswitch-vpn_1.0.0_all.deb
