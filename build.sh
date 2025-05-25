ec2c3c3c03671b98467d0d18a00cb2934808b7bf
Matthew Elgert <mdelgert@yahoo.com>
2025-05-24 22:13:36 -0400

    Enhance prerm script to remove empty directories and update build.sh for proper package building

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-dpkg-deb --build --root-owner-group .
+
+dpkg-deb --build --root-owner-group . nsswitch-vpn_1.0.0.deb
+chmod 644 nsswitch-vpn_1.0.0.deb
 sudo apt install ./nsswitch-vpn_1.0.0.deb
-nmcli connection up WireGuard
-ssh b1.local
-nmcli connection down WireGuard
-sudo apt remove nsswitch-vpn
+# nmcli connection up WireGuard
+# nmcli connection down WireGuard
+# sudo apt remove nsswitch-vpn
+# rm nsswitch-vpn_1.0.0.deb
