93727925949671870455e1cd1c037ac885cd749b
Matthew Elgert <mdelgert@yahoo.com>
2025-05-24 22:38:47 -0400

    Refactor prerm and build scripts to streamline cleanup and installation processes

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-
+rm nsswitch-vpn_1.0.0.deb
+sudo apt remove nsswitch-vpn -y
 dpkg-deb --build --root-owner-group . nsswitch-vpn_1.0.0.deb
 chmod 644 nsswitch-vpn_1.0.0.deb
 sudo apt install ./nsswitch-vpn_1.0.0.deb
 # nmcli connection up WireGuard
 # nmcli connection down WireGuard
-# sudo apt remove nsswitch-vpn
-# rm nsswitch-vpn_1.0.0.deb
