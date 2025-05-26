3730abb6a8cd03cd58e7f6016b57d8f96444094a
Matthew Elgert <mdelgert@yahoo.com>
2025-05-26 23:33:56 +0000

    Bump version to v1.0.3 in documentation, source files, and build scripts

--- tools/build.sh
+++ tools/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # This script builds the package using debuild and moves all build artifacts to ./build-artifacts
+# Location: tools/build.sh
+# Usage: ./tools/build.sh
+
+set -e
 
 # Prevent running as root/sudo
 if [ "$EUID" -eq 0 ]; then
@@ -8,17 +12,13 @@ if [ "$EUID" -eq 0 ]; then
   exit 1
 fi
 
-set -e
-
-ls -l
 cd ../
+sudo chown -R $(id -u):$(id -g) ../
 debuild -us -uc
 rm -rf build-artifacts
 mkdir -p build-artifacts
-ls -l ../
 mv ../vpn-nsswitch_* build-artifacts/
 mv ../vpn-nsswitch-* build-artifacts/
-ls -l build-artifacts
-# lintian build-artifacts/*.deb
+lintian build-artifacts/*.deb
 
 exit 0
