a70f6b5948f45a5bebe5bdfa506989ec0398d08d
Matthew Elgert <mdelgert@yahoo.com>
2025-05-25 23:15:07 -0400

    Update clean script to remove specific debian helper files and add copyright file

--- tools/clean.sh
+++ tools/clean.sh
@@ -4,7 +4,7 @@ cd ../
 
 rm vpn-nsswitch
 rm -rf build-artifacts/
-rm -rf debian/.debhelper/
+rm -rf debian/.debhelper.*
+rm -rf debian/nsswitch-vpn.*
 rm -rf debian/vpn-nsswitch/
 rm -rf debian/files
-rm -rf debian/vpn-nsswitch.*
