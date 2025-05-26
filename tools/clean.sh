8f7b456d6b4802dfde71e6b79c36c33f7db7a0d7
Matthew Elgert <mdelgert@yahoo.com>
2025-05-25 23:20:13 -0400

    Enhance clean script to enforce root execution and refine file removal patterns

--- tools/clean.sh
+++ tools/clean.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# Check if script is run as root (sudo)
+if [ "$EUID" -ne 0 ]; then
+    echo "Error: This script must be run as root (use sudo)." >&2
+    exit 1
+fi
+
 cd ../
 
 rm vpn-nsswitch
 rm -rf build-artifacts/
-rm -rf debian/.debhelper.*
-rm -rf debian/nsswitch-vpn.*
+rm -rf debian/.debhelper/
+rm -rf debian/debhelper*
+rm -rf debian/nsswitch-vpn*
 rm -rf debian/vpn-nsswitch/
 rm -rf debian/files
