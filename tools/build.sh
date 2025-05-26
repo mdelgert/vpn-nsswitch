3beb03637aecae3b6cb9e62e668d2d699fdf98fe
Matthew Elgert <mdelgert@yahoo.com>
2025-05-26 18:33:49 -0400

    Update vpn-nsswitch man page and build script for clarity and accuracy

--- tools/build.sh
+++ tools/build.sh
@@ -2,6 +2,12 @@
 
 # This script builds the package using debuild and moves all build artifacts to ./build-artifacts
 
+# Prevent running as root/sudo
+if [ "$EUID" -eq 0 ]; then
+  echo "Do not run this script as root or with sudo."
+  exit 1
+fi
+
 set -e
 
 ls -l
@@ -9,8 +15,10 @@ cd ../
 debuild -us -uc
 rm -rf build-artifacts
 mkdir -p build-artifacts
-mv ../vpn-nsswitch* build-artifacts/
+ls -l ../
+mv ../vpn-nsswitch_* build-artifacts/
+mv ../vpn-nsswitch-* build-artifacts/
 ls -l build-artifacts
-lintian build-artifacts/*.deb
+# lintian build-artifacts/*.deb
 
 exit 0
