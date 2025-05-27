b60b8df9b44676daf14619d1665e019f4a2dc385
Matthew Elgert <mdelgert@yahoo.com>
2025-05-27 00:44:01 +0000

    Refactor clean and build scripts to improve organization and clarity

--- tools/build.sh
+++ tools/build.sh
@@ -6,7 +6,9 @@ if [ "$EUID" -eq 0 ]; then
   exit 1
 fi
 
+./clean.sh
 cd ../
 debuild -us -uc
+mkdir -p build-artifacts 2>/dev/null
 mv ../vpn-nsswitch_* build-artifacts/
 lintian build-artifacts/*.deb
--- tools/clean.sh
+++ tools/clean.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-# Check if script is run as root (sudo)
-if [ "$EUID" -ne 0 ]; then
-    echo "Error: This script must be run as root (use sudo)." >&2
-    exit 1
+if [ "$EUID" -eq 0 ]; then
+  echo "Do not run as root"
+  exit 1
 fi
 
 cd ../
-
-# Remove the debian package
-rm vpn-nsswitch
-rm -rf build-artifacts/
-rm -rf debian/.debhelper/
-rm -rf debian/debhelper*
-rm -rf debian/vpn-nsswitch/
-rm -rf debian/files
-
-# Remove installed files
-# rm -f /var/log/nsswitch_script.log
-# rm -rf /etc/nsswitch.d
-# rm /usr/bin/nsswitch_up.sh
-# rm /usr/bin/nsswitch_down.sh
-# rm /etc/NetworkManager/dispatcher.d/vpn-nsswitch-openvpn.sh
-# rm /etc/NetworkManager/dispatcher.d/vpn-nsswitch-wireguard.sh
+sudo rm -f vpn-nsswitch 2>/dev/null
+sudo rm -rf build-artifacts/ 2>/dev/null
+sudo rm -rf debian/.debhelper/ 2>/dev/null
+sudo rm -rf debian/debhelper* 2>/dev/null
+sudo rm -rf debian/vpn-nsswitch/ 2>/dev/null
+sudo rm -rf debian/files 2>/dev/null
