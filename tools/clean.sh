b32358aef9eda4d4c7e120619f7b66c34ef81d4b
Matthew Elgert <mdelgert@yahoo.com>
2025-05-27 05:04:30 +0000

    Remove additional debian/vpn-nsswitch.substvars during cleanup process

--- tools/clean.sh
+++ tools/clean.sh
@@ -12,6 +12,7 @@ sudo rm -rf build-artifacts/ 2>/dev/null
 sudo rm -rf debian/.debhelper/ 2>/dev/null
 sudo rm -rf debian/debhelper* 2>/dev/null
 sudo rm -rf debian/vpn-nsswitch/ 2>/dev/null
+sudo rm -rf debian/vpn-nsswitch.substvars 2>/dev/null
 sudo rm -rf debian/files 2>/dev/null
 
 exit 0
