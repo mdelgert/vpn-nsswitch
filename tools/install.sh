514b5de1a9298ab314d97096c411639eff3213d3
Matthew Elgert <mdelgert@yahoo.com>
2025-05-26 23:45:28 -0400

    Improve installation script by adding root user check and ensuring consistent command usage

--- tools/install.sh
+++ tools/install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-if [ "$EUID" -eq 0 ]; then
-  echo "Do not run as root"
-  exit 1
-fi
-
 # Clean previous install if it exists
 sudo apt purge -y vpn-nsswitch || true
 
-wget -O vpn-nsswitch.deb $(curl -s https://api.github.com/repos/mdelgert/vpn-nsswitch/releases/latest | grep "browser_download_url.*deb" | cut -d '"' -f 4)
+# Download latest release .deb
+wget -O vpn-nsswitch.deb $(curl -s https://api.github.com/repos/mdelgert/vpn-nsswitch/releases/latest \
+  | grep "browser_download_url.*\.deb" \
+  | cut -d '"' -f 4)
 
+# Install .deb
 sudo dpkg -i vpn-nsswitch.deb
 
-sudo apt-get install -f
+# Fix missing dependencies
+sudo apt-get install -f -y
 
 exit 0
